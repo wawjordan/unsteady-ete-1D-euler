5d9c9ab5a48dd599b954d8bb7ff308c30d4d7803
wawjordan <wajordan@vt.edu>
2021-08-10 09:42:36 -0400

    Added additional integer and fractional constants, as well as large and small values

--- src/constants/set_constants.f90
+++ src/constants/set_constants.f90
@@ -6,23 +6,36 @@ module set_constants
 
   private
 
-  public :: zero, one, two, three, four, six
-  public :: half, third, fourth, fifth, sixth, tenth
-  public :: pi, set_derived_constants
+  public :: zero, one, two, three, four, five, six, seven, eight, nine, ten
+  public :: half, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth
+  public :: small, large, pi, set_derived_constants
 
-  real(prec), parameter :: zero   = 0.0_prec
-  real(prec), parameter :: tenth  = 0.1_prec
-  real(prec), parameter :: sixth  = 1.0_prec/6.0_prec
-  real(prec), parameter :: fifth  = 0.2_prec
-  real(prec), parameter :: fourth = 0.25_prec
-  real(prec), parameter :: third  = 1.0_prec/3.0_prec
-  real(prec), parameter :: half   = 0.5_prec
-  real(prec), parameter :: one    = 1.0_prec
-  real(prec), parameter :: two    = 2.0_prec
-  real(prec), parameter :: three  = 3.0_prec
-  real(prec), parameter :: four   = 4.0_prec
-  real(prec), parameter :: six    = 6.0_prec
-  real(prec)            :: pi     = 3.0_prec
+  real(prec), parameter :: zero    = 0.0_prec
+  real(prec), parameter :: one     = 1.0_prec
+  real(prec), parameter :: two     = 2.0_prec
+  real(prec), parameter :: three   = 3.0_prec
+  real(prec), parameter :: four    = 4.0_prec
+  real(prec), parameter :: five    = 5.0_prec
+  real(prec), parameter :: six     = 6.0_prec
+  real(prec), parameter :: seven   = 7.0_prec
+  real(prec), parameter :: eight   = 8.0_prec
+  real(prec), parameter :: nine    = 9.0_prec
+  real(prec), parameter :: ten     = 10.0_prec
+
+  real(prec), parameter :: half    = 0.5_prec
+  real(prec), parameter :: third   = 1.0_prec/3.0_prec
+  real(prec), parameter :: fourth  = 0.25_prec
+  real(prec), parameter :: fifth   = 0.2_prec
+  real(prec), parameter :: sixth   = 1.0_prec/6.0_prec
+  real(prec), parameter :: seventh = 1.0_prec/7.0_prec
+  real(prec), parameter :: eighth  = 0.125_prec
+  real(prec), parameter :: ninth   = 1.0_prec/9.0_prec
+  real(prec), parameter :: tenth   = 0.1_prec
+
+  real(prec), parameter :: small   = 0.99e-30_prec
+  real(prec), parameter :: large   = 0.99e+30_prec
+
+  real(prec)            :: pi      = 3.0_prec
 
   contains
 
