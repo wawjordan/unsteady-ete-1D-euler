3ff99831bf6ca7ada0ea3c61c27e132f7c7f426b
wawjordan <wajordan@vt.edu>
2021-08-11 16:30:50 -0400

    Added the following variables near_zero, pix180 GMRES_SOLVER, CG_SOLVER, CGNR_SOLVER, BICG_SOLVER, CGS_SOLVER, BICGSTAB_SOLVER NO_PRECOND, ILU0_PRECOND, ILUK_PRECOND, ILUT_PRECOND, ILUTP_PRECOND LEFT_PRECOND, RIGHT_PRECOND, SPLIT_PRECOND

--- src/constants/set_constants.f90
+++ src/constants/set_constants.f90
@@ -8,7 +8,12 @@ module set_constants
 
   public :: zero, one, two, three, four, five, six, seven, eight, nine, ten
   public :: half, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth
-  public :: small, large, pi, set_derived_constants
+  public :: near_zero, small, large, pi, pix180, set_derived_constants
+
+  public :: GMRES_SOLVER, CG_SOLVER, CGNR_SOLVER, CGNE_SOLVER, BICG_SOLVER,    &
+            CGS_SOLVER, BICGSTAB_SOLVER
+  public :: NO_PRECOND, ILU0_PRECOND, ILUK_PRECOND, ILUT_PRECOND, ILUTP_PRECOND
+  public :: LEFT_PRECOND, RIGHT_PRECOND, SPLIT_PRECOND
 
   real(prec), parameter :: zero    = 0.0_prec
   real(prec), parameter :: one     = 1.0_prec
@@ -32,10 +37,36 @@ module set_constants
   real(prec), parameter :: ninth   = 1.0_prec/9.0_prec
   real(prec), parameter :: tenth   = 0.1_prec
 
-  real(prec), parameter :: small   = 0.99e-30_prec
-  real(prec), parameter :: large   = 0.99e+30_prec
+  real(prec), parameter :: near_zero = epsilon(one)
+  real(prec), parameter :: small   = tiny(one) !0.99e-30_prec
+  real(prec), parameter :: large   = huge(one) !0.99e+30_prec
 
   real(prec)            :: pi      = 3.0_prec
+  real(prec)            :: pix180  = 0.0_prec
+
+
+
+  ! Linear solver options
+  integer, parameter :: GMRES_SOLVER    = 70
+  integer, parameter :: CG_SOLVER       = 71 ! To be implemented
+  integer, parameter :: CGNR_SOLVER     = 72 ! To be implemented
+  integer, parameter :: CGNE_SOLVER     = 73 ! To be implemented
+  integer, parameter :: BICG_SOLVER     = 74 ! To be implemented
+  integer, parameter :: CGS_SOLVER      = 75 ! To be implemented
+  integer, parameter :: BICGSTAB_SOLVER = 76 ! To be implemented
+
+
+  ! Preconditioner options
+  integer, parameter :: NO_PRECOND    = 120
+  integer, parameter :: ILU0_PRECOND  = 121
+  integer, parameter :: ILUK_PRECOND  = 122
+  integer, parameter :: ILUT_PRECOND  = 123 ! To be implemented
+  integer, parameter :: ILUTP_PRECOND = 124
+
+  ! Preconditioner side options
+  integer, parameter :: LEFT_PRECOND  = 130
+  integer, parameter :: RIGHT_PRECOND = 131
+  integer, parameter :: SPLIT_PRECOND = 132
 
   contains
 
@@ -49,6 +80,7 @@ module set_constants
       implicit none
 
       pi = acos(-1.0_prec)
+      pix180 = pi/180.0_prec
 
     end subroutine set_derived_constants
 
