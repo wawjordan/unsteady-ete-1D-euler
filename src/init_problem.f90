d71e1c515da6707b8fa988eba8e4c0a97ec8e99f
wawjordan <wajordan@vt.edu>
2021-08-11 09:54:30 -0400

    Added U_L and U_R inputs to replace hard-coded values

--- src/init_problem.f90
+++ src/init_problem.f90
@@ -7,6 +7,7 @@ module init_problem
   use soln_type
   use grid_type
   use variable_conversion
+  use riemann_problem, only : init_bracket, solve_riemann, cell_avg_riemann
 
   implicit none
   private
@@ -25,32 +26,33 @@ module init_problem
   !!              soln :
   !<
   !===========================================================================80
-  subroutine initialize( grid, soln )
+  subroutine initialize( grid, soln, U_L, U_R )
 
     implicit none
 
     integer :: i
     type( soln_t ), intent(inout) :: soln
     type( grid_t ), intent(inout) :: grid
+    real(prec), intent(in) :: U_L(neq), U_R(neq)
+    real(prec) :: p0, bnd_a, bnd_b, pStar, uStar, xstar, t
 
     where(grid%xc<=0.0_prec)
-      soln%V(:,1) = one
-      soln%V(:,2) = zero
-      soln%V(:,3) = 100000.0_prec
+      soln%V(:,1) = U_L(1)
+      soln%V(:,2) = U_L(2)
+      soln%V(:,3) = U_L(3)
     elsewhere
-      soln%V(:,1) = 0.125_prec
-      soln%V(:,2) = zero
-      soln%V(:,3) = 10000.0_prec
+      soln%V(:,1) = U_R(1)
+      soln%V(:,2) = U_R(2)
+      soln%V(:,3) = U_R(3)
     end where
-    !do i = ig_low,ig_high
-    !  if ( soln%mach(i) < eps ) then
-    !    soln%mach(i) = eps
-    !  end if
-    !end do
 
-    !call isentropic_relations(soln%mach,soln%V)
+    !call init_bracket(U_L,U_R,1.0e-6_prec,p0,bnd_a,bnd_b)
+    !call solve_riemann(&
+    !  p0,bnd_a,bnd_b,1.0e-16_prec,100,U_L,U_R,pStar,uStar)
+
     call prim2cons(soln%U,soln%V)
     call update_states( soln )
+
 
   end subroutine initialize
 
