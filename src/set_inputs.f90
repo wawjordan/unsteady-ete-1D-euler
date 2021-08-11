5a84d251131a0a09036981c591a98881a86e47b3
wawjordan <wajordan@vt.edu>
2021-08-11 10:55:31 -0400

    removed obsolete variables

--- src/set_inputs.f90
+++ src/set_inputs.f90
@@ -11,10 +11,10 @@ module set_inputs
   public :: imax, neq, xmin, xmax, n_ghost_cells
   public :: i_high, i_low, ig_high, ig_low
   public :: areaStar, area, darea
-  public :: CFL, k2, k4, eps, tol, eps_roe, beta_lim, epsM, kappaM
+  public :: CFL, eps, tol, eps_roe, beta_lim, epsM, kappaM
   public :: max_iter, max_newton_iter, newton_tol, counter
-  public :: iSS, shock, ramp, soln_save, res_save, res_out
-  public :: p0, T0, a0, rho0, pb, p_ratio
+  public :: soln_save, res_save, res_out
+  public :: p0, T0, a0, rho0
   public :: set_derived_inputs, flux_scheme, limiter_scheme, cons
   public :: leftV, rightV, leftU, rightU, limiter_freeze, psi_plus, psi_minus
 
@@ -24,28 +24,21 @@ module set_inputs
   integer :: ig_low  = 10
   integer :: ig_high = 10
   integer :: neq  = 3
-  integer :: iSS  = 1
-  integer :: shock = 0
-  integer :: ramp = 0
   integer :: max_iter = 150000
   integer :: n_ghost_cells   = 2
   integer :: max_newton_iter = 1000
 
   real(prec) :: newton_tol = 1.0e-15_prec
-  real(prec) :: tol = 1.0e-11_prec
+  real(prec) :: tol        = 1.0e-11_prec
   real(prec) :: eps        = 1.0e-3_prec
-  real(prec) :: p0         = 300.0_prec
-  real(prec) :: T0         = 600.0_prec
-  real(prec) :: areaStar      = 0.2_prec
-  real(prec) :: a0         = zero
-  real(prec) :: rho0       = zero
+  real(prec) :: p0         = one
+  real(prec) :: T0         = one
+  real(prec) :: a0         = one
+  real(prec) :: rho0       = one
+  real(prec) :: areaStar   = 0.2_prec
   real(prec) :: xmin       = -one
   real(prec) :: xmax       = one
   real(prec) :: CFL        = 0.1_prec
-  real(prec) :: p_ratio    = 0.5_prec
-  real(prec) :: pb         = 150000_prec
-  real(prec) :: k2         = 1.0_prec/2.0_prec
-  real(prec) :: k4         = 1.0_prec/32.0_prec
   integer :: flux_scheme   = 1
   integer :: limiter_scheme = 2
   real(prec) :: beta_lim = 2
@@ -111,7 +104,6 @@ module set_inputs
     i_high = imax
     ig_low  = 1 - n_ghost_cells
     ig_high = imax + n_ghost_cells
-    pb = p_ratio*p0*1000_prec
     write(*,'(A8,F20.14,A13)') 'R     = ', R_gas, ' [J/(kmol*K)]'
     write(*,'(A8,F20.14)')     'gamma = ', gamma
     write(*,'(A8,F20.14,A6)')  'a_0   = ', a0, ' [m/s]'
@@ -124,8 +116,6 @@ module set_inputs
     allocate(rightV(i_low-1:i_high,1:neq))
     allocate(leftU(i_low-1:i_high,1:neq))
     allocate(rightU(i_low-1:i_high,1:neq))
-    !allocate(psi_plus(i_low-1:i_high,1:neq))
-    !allocate(psi_minus(i_low-1:i_high,1:neq))
     allocate(psi_plus(ig_low:ig_high,1:neq))
     allocate(psi_minus(ig_low:ig_high,1:neq))
 
