17c9e5c24a21de6cb288b17d1a68167facfed06a
wawjordan <wajordan@vt.edu>
2021-08-25 09:50:52 -0400

    Added array for dx (preparation for non-uniform grid)

--- src/types/grid_type.f90
+++ src/types/grid_type.f90
@@ -10,10 +10,10 @@ module grid_type
 
     real(prec) :: xmin
     real(prec) :: xmax
-    real(prec) :: dx
 
     real(prec), allocatable, dimension(:) :: xi
     real(prec), allocatable, dimension(:) :: xc
+    real(prec), allocatable, dimension(:) :: dx
     real(prec), allocatable, dimension(:) :: Ai
     real(prec), allocatable, dimension(:) :: Ac
     real(prec), allocatable, dimension(:) :: dAc
@@ -30,27 +30,95 @@ module grid_type
   subroutine allocate_grid( grid )
 
     use set_constants, only : zero, one, half
-    use set_inputs   , only : imax, xmin, xmax, ig_low, ig_high, i_low, i_high
+    use set_inputs   , only : imax, ig_low, ig_high, i_low, i_high, &
+                              n_ghost, xmin, xmax
 
     type( grid_t ), intent( inout ) :: grid
     integer :: i
 
-    grid%dx = (xmax - xmin)/real(imax,prec)
-
     allocate( grid%xi(ig_low-1:ig_high), &
               grid%xc(ig_low:ig_high)  , &
+              grid%dx(ig_low:ig_high)  , &
               grid%Ai(ig_low-1:ig_high), &
               grid%Ac(ig_low:ig_high)  , &
               grid%dAc(i_low:i_high)   )
 
+    grid%dx = (xmax - xmin)/real(imax,prec)
+
+
     grid%xi = [ (xmin + real(i,prec)/real(imax,prec)*(xmax-xmin), &
                i=ig_low-1,ig_high) ]
     grid%xc = [ (half*(grid%xi(i) + grid%xi(i+1)),i=ig_low-1,ig_high-1) ]
+    !do i = ig_low,ig_high
+    !  write(*,'(I3,3G12.4)') i, grid%xi(i), grid%xc(i), grid%dx(i)
+    !end do
+
     grid%Ai = one
     grid%Ac = one
     grid%dAc = zero
 
   end subroutine allocate_grid
+!  subroutine allocate_grid( grid, xi )
+!
+!    use set_constants, only : zero, one, half
+!    use set_inputs   , only : imax, ig_low, ig_high, i_low, i_high, &
+!                              n_ghost, xmin, xmax
+!
+!    type( grid_t ), intent( inout ) :: grid
+!    real(prec), dimension(:), intent(in) :: xi
+!    real(prec) :: dx_left, dx_right
+!    integer :: i, nmax
+!
+!    nmax = size(xi)
+!    if (i /= imax) then
+!      imax    = nmax-1
+!      i_low   = 1
+!      i_high  = imax
+!      ig_low  = 1 - n_ghost
+!      ig_high = imax + n_ghost
+!    end if
+!    xmin = minval(xi)
+!    xmax = maxval(xi)
+!
+!    allocate( grid%xi(ig_low-1:ig_high), &
+!              grid%xc(ig_low:ig_high)  , &
+!              grid%dx(ig_low:ig_high)  , &
+!              grid%Ai(ig_low-1:ig_high), &
+!              grid%Ac(ig_low:ig_high)  , &
+!              grid%dAc(i_low:i_high)   )
+!
+!    grid%xi = zero
+!
+!    grid%xi(i_low-1:i_high) = xi
+!
+!    grid%dx(i_low:i_high) = xi(2:nmax) - xi(1:nmax-1)
+!
+!    grid%dx(i_low-1:ig_low:-1) = grid%dx(i_low)
+!    grid%dx(i_high+1:ig_high) = grid%dx(i_high)
+!
+!
+!    do i = i_low-1,ig_low,-1
+!      grid%xi(i-1) = grid%xi(i) - grid%dx(i_low)
+!    end do
+!    do i = i_high+1,ig_high
+!      grid%xi(i) = grid%xi(i-1) + grid%dx(i_high)
+!    end do
+!
+!    !grid%dx = (xmax - xmin)/real(imax,prec)
+!
+!
+!    !grid%xi = [ (xmin + real(i,prec)/real(imax,prec)*(xmax-xmin), &
+!    !           i=ig_low-1,ig_high) ]
+!    grid%xc = [ (half*(grid%xi(i) + grid%xi(i+1)),i=ig_low-1,ig_high-1) ]
+!    !do i = ig_low,ig_high
+!    !  write(*,'(I3,3G12.4)') i, grid%xi(i), grid%xc(i), grid%dx(i)
+!    !end do
+!
+!    grid%Ai = one
+!    grid%Ac = one
+!    grid%dAc = zero
+!
+!  end subroutine allocate_grid
 
   !============================= deallocate_grid =============================80
   !>
