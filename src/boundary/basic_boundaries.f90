0cb22fe693c210a2097fba6bb77af2b5855aae84
wawjordan <wajordan@vt.edu>
2021-08-25 09:26:27 -0400

    Swapped array dimensions Added BC strings for debugging Changed subsonic outflow BC (mostly fixed)

--- src/boundary/basic_boundaries.f90
+++ src/boundary/basic_boundaries.f90
@@ -25,48 +25,59 @@ contains
     logical :: flag
     integer :: i
 
+    character(9) :: str1, str2
+
     flag = .false.
 
     ! Left boundary
-    rho  = soln%V(i_low,1)
-    uvel = soln%V(i_low,2)
-    pres = soln%V(i_low,3)
+    rho  = soln%V(1,i_low)
+    uvel = soln%V(2,i_low)
+    pres = soln%V(3,i_low)
     avel = soln%asnd(i_low)
     if ( uvel < zero ) then ! inflow
       if ( abs(uvel) >= avel ) then ! supersonic
         call Left_sup_in_bndry(soln,VL)
+        str1 = 'SUP_L_IN'
       else ! subsonic
         call Left_sub_in_bndry(soln,VL)
         flag = .true.
+        str1 = 'SUB_L_IN'
       end if
     else ! outflow
       if ( abs(uvel) >= avel ) then ! supersonic
         call Left_sup_out_bndry(soln,VL)
+        str1 = 'SUP_L_OUT'
       else ! subsonic
         call Left_sub_out_bndry(soln,VL)
         flag = .true.
+        str1 = 'SUB_L_OUT'
       end if
     end if
 
-
     ! Right boundary
-    rho  = soln%V(i_high,1)
-    uvel = soln%V(i_high,2)
-    pres = soln%V(i_high,3)
+    rho  = soln%V(1,i_high)
+    uvel = soln%V(2,i_high)
+    pres = soln%V(3,i_high)
     avel = soln%asnd(i_high)
     if ( uvel < zero ) then ! inflow
       if ( abs(uvel) >= avel ) then ! supersonic
         call Right_sup_in_bndry(soln,VR)
+        str2 = 'SUP_R_IN'
       else ! subsonic
         call Right_sub_in_bndry(soln,VR,flag)
+        str2 = 'SUB_R_IN'
       end if
     else ! outflow
       if ( abs(uvel) >= avel ) then ! supersonic
         call Right_sup_out_bndry(soln,VR)
+        str2 = 'SUP_R_OUT'
       else ! subsonic
         call Right_sub_out_bndry(soln,VR,flag)
+        str2 = 'SUB_R_OUT'
       end if
     end if
+
+    !write(*,'(F10.6,A3,A9,A2,A9)') soln%time,' : ',str1,', ', str2
 
   end subroutine explicit_characteristic_bndry
 
@@ -79,16 +90,16 @@ contains
     real(prec), dimension(2) :: tmp
     integer :: i
 
-    rho  = soln%V(i_low,1)
-    uvel = soln%V(i_low,2)
-    pres = soln%V(i_low,3)
+    rho  = soln%V(1,i_low)
+    uvel = soln%V(2,i_low)
+    pres = soln%V(3,i_low)
     avel = soln%asnd(i_low)
 
     ! left subsonic inflow
     R0_ghost = riemann_1(Vspec(1),Vspec(3))
     Rp_ghost = riemann_2(Vspec(1),uvel,avel)
     tmp(1)   = riemann_3(rho,uvel,avel)
-    tmp(2)   = riemann_3(soln%V(i_low+1,1),soln%V(i_low+1,2),soln%asnd(i_low+1))
+    tmp(2)   = riemann_3(soln%V(1,i_low+1),soln%V(2,i_low+1),soln%asnd(i_low+1))
     do i = i_low-1,ig_low,-1
       Rm_ghost = two*tmp(1) - tmp(2)
       tmp(2) = tmp(1)
@@ -97,7 +108,7 @@ contains
       call riem2prim(Rp_ghost,Rm_ghost,R0_ghost, &
             rho_ghost,uvel_ghost,avel_ghost,pres_ghost)
 
-      soln%V(i,:) = (/ rho_ghost, uvel_ghost, pres_ghost /)
+      soln%V(:,i) = (/ rho_ghost, uvel_ghost, pres_ghost /)
       soln%asnd(i) = avel_ghost
     end do
   end subroutine Left_sub_in_bndry
@@ -112,9 +123,9 @@ contains
     logical :: flag
     integer :: i
 
-    rho  = soln%V(i_high,1)
-    uvel = soln%V(i_high,2)
-    pres = soln%V(i_high,3)
+    rho  = soln%V(1,i_high)
+    uvel = soln%V(2,i_high)
+    pres = soln%V(3,i_high)
     avel = soln%asnd(i_high)
 
     ! right subsonic inflow
@@ -126,8 +137,8 @@ contains
       Rp_ghost = riemann_2(Vspec(1),Vspec(2),avel)
     end if
     tmp(1)   = riemann_3(rho,uvel,avel)
-    tmp(2)   = riemann_3(soln%V(i_high-1,1),&
-                         soln%V(i_high-1,2),soln%asnd(i_high-1))
+    tmp(2)   = riemann_3(soln%V(1,i_high-1),&
+                         soln%V(2,i_high-1),soln%asnd(i_high-1))
     do i = i_high+1,ig_high
       Rm_ghost = two*tmp(1) - tmp(2)
       tmp(2) = tmp(1)
@@ -136,7 +147,7 @@ contains
       call riem2prim(Rp_ghost,Rm_ghost,R0_ghost, &
             rho_ghost,uvel_ghost,avel_ghost,pres_ghost)
 
-      soln%V(i,:) = (/ rho_ghost, uvel_ghost, pres_ghost /)
+      soln%V(:,i) = (/ rho_ghost, uvel_ghost, pres_ghost /)
       soln%asnd(i) = avel_ghost
     end do
   end subroutine Right_sub_in_bndry
@@ -144,25 +155,27 @@ contains
   subroutine Left_sub_out_bndry(soln,Vspec)
     type(soln_t), intent(inout) :: soln
     real(prec), dimension(neq), intent(in) :: Vspec
-    real(prec) :: rho, uvel, avel, pres
+    real(prec) :: rho, uvel, avel, pres, aspec
     real(prec) :: rho_ghost, uvel_ghost, avel_ghost, pres_ghost
     real(prec) :: Rp_ghost, Rm_ghost, R0_ghost
     real(prec), dimension(2) :: tmp1, tmp2
     logical :: flag
     integer :: i
 
-    rho  = soln%V(i_low,1)
-    uvel = soln%V(i_low,2)
-    pres = soln%V(i_low,3)
+    rho  = soln%V(1,i_low)
+    uvel = soln%V(2,i_low)
+    pres = soln%V(3,i_low)
     avel = soln%asnd(i_low)
 
+    call speed_of_sound(Vspec(3),rho,aspec)
+
     ! left subsonic outflow
-    Rm_ghost = riemann_3(rho,uvel,Vspec(3))
+    Rm_ghost = riemann_3(rho,uvel,aspec)
 
     tmp1(1)  = riemann_1(rho,pres)
-    tmp1(2)  = riemann_1(soln%V(i_low+1,1),soln%V(i_low+1,3))
+    tmp1(2)  = riemann_1(soln%V(1,i_low+1),soln%V(3,i_low+1))
     tmp2(1)  = riemann_2(rho,uvel,avel)
-    tmp2(2)  = riemann_2(soln%V(i_low+1,1),soln%V(i_low+1,2),soln%asnd(i_low+1))
+    tmp2(2)  = riemann_2(soln%V(1,i_low+1),soln%V(2,i_low+1),soln%asnd(i_low+1))
 
     do i = i_low-1,ig_low,-1
       R0_ghost = two*tmp1(1) - tmp1(2)
@@ -175,7 +188,7 @@ contains
       call riem2prim(Rp_ghost,Rm_ghost,R0_ghost, &
             rho_ghost,uvel_ghost,avel_ghost,pres_ghost)
 
-      soln%V(i,:) = (/ rho_ghost, uvel_ghost, pres_ghost /)
+      soln%V(:,i) = (/ rho_ghost, uvel_ghost, pres_ghost /)
       soln%asnd(i) = avel_ghost
     end do
   end subroutine Left_sub_out_bndry
@@ -183,33 +196,34 @@ contains
   subroutine Right_sub_out_bndry(soln,Vspec,flag)
     type(soln_t), intent(inout) :: soln
     real(prec), dimension(neq), intent(in) :: Vspec
-    real(prec) :: rho, uvel, avel, pres
+    real(prec) :: rho, uvel, avel, pres, aspec
     real(prec) :: rho_ghost, uvel_ghost, avel_ghost, pres_ghost
     real(prec) :: Rp_ghost, Rm_ghost, R0_ghost
     real(prec), dimension(2) :: tmp1, tmp2
     logical :: flag
     integer :: i
 
-    rho  = soln%V(i_high,1)
-    uvel = soln%V(i_high,2)
-    pres = soln%V(i_high,3)
+    rho  = soln%V(1,i_high)
+    uvel = soln%V(2,i_high)
+    pres = soln%V(3,i_high)
     avel = soln%asnd(i_high)
+    call speed_of_sound(Vspec(3),rho,aspec)
 
     ! right subsonic outflow
     if (flag.eqv..false.) then
-      Rm_ghost = riemann_3(rho,uvel,Vspec(3))
+      Rm_ghost = riemann_3(rho,uvel,aspec)
     else
-      Rm_ghost = riemann_3(Vspec(1),uvel,pres)
+      Rm_ghost = riemann_3(rho,Vspec(2),avel)
     end if
 
     tmp1(1)  = riemann_1(rho,pres)
-    tmp1(2)  = riemann_1(soln%V(i_high-1,1),&
-                         soln%V(i_high-1,3))
+    tmp1(2)  = riemann_1(soln%V(1,i_high-1),&
+                         soln%V(3,i_high-1))
     tmp2(1)  = riemann_2(rho,uvel,avel)
-    tmp2(2)  = riemann_2(soln%V(i_high-1,1),&
-                         soln%V(i_high-1,2),soln%asnd(i_high-1))
+    tmp2(2)  = riemann_2(soln%V(1,i_high-1),&
+                         soln%V(2,i_high-1),soln%asnd(i_high-1))
 
-    do i = i_low-1,ig_low,-1
+    do i = i_high+1,ig_high
       R0_ghost = two*tmp1(1) - tmp1(2)
       Rp_ghost = two*tmp2(1) - tmp2(2)
       tmp1(2) = tmp1(1)
@@ -220,7 +234,7 @@ contains
       call riem2prim(Rp_ghost,Rm_ghost,R0_ghost, &
             rho_ghost,uvel_ghost,avel_ghost,pres_ghost)
 
-      soln%V(i,:) = (/ rho_ghost, uvel_ghost, pres_ghost /)
+      soln%V(:,i) = (/ rho_ghost, uvel_ghost, pres_ghost /)
       soln%asnd(i) = avel_ghost
     end do
   end subroutine Right_sub_out_bndry
@@ -246,7 +260,7 @@ contains
           rho_ghost,uvel_ghost,avel_ghost,pres_ghost)
 
     do i = i_low-1,ig_low,-1
-      soln%V(i,:) = (/ rho_ghost, uvel_ghost, pres_ghost /)
+      soln%V(:,i) = (/ rho_ghost, uvel_ghost, pres_ghost /)
       soln%asnd(i) = avel_ghost
     end do
   end subroutine Left_sup_in_bndry
@@ -272,7 +286,7 @@ contains
           rho_ghost,uvel_ghost,avel_ghost,pres_ghost)
 
     do i = i_high+1,ig_high
-      soln%V(i,:) = (/ rho_ghost, uvel_ghost, pres_ghost /)
+      soln%V(:,i) = (/ rho_ghost, uvel_ghost, pres_ghost /)
       soln%asnd(i) = avel_ghost
     end do
   end subroutine Right_sup_in_bndry
@@ -291,19 +305,19 @@ contains
     pres = Vspec(3)
     call speed_of_sound(pres,rho,avel)
 
-    ! left supersonic inflow
-    tmp1(1)  = riemann_1(soln%V(i_low,1),&
-                         soln%V(i_low,3))
-    tmp1(2)  = riemann_1(soln%V(i_low+1,1),&
-                         soln%V(i_low+1,3))
-    tmp2(1)  = riemann_2(soln%V(i_low,1),&
-                         soln%V(i_low,2),soln%asnd(i_low))
-    tmp2(2)  = riemann_2(soln%V(i_low+1,1),&
-                         soln%V(i_low+1,2),soln%asnd(i_low+1))
-    tmp3(1)  = riemann_3(soln%V(i_low,1),&
-                         soln%V(i_low,2),soln%asnd(i_low))
-    tmp3(2)  = riemann_3(soln%V(i_low+1,1),&
-                         soln%V(i_low+1,2),soln%asnd(i_low+1))
+    ! left supersonic outflow
+    tmp1(1)  = riemann_1(soln%V(1,i_low),&
+                         soln%V(3,i_low))
+    tmp1(2)  = riemann_1(soln%V(1,i_low+1),&
+                         soln%V(3,i_low+1))
+    tmp2(1)  = riemann_2(soln%V(1,i_low),&
+                         soln%V(2,i_low),soln%asnd(i_low))
+    tmp2(2)  = riemann_2(soln%V(1,i_low+1),&
+                         soln%V(2,i_low+1),soln%asnd(i_low+1))
+    tmp3(1)  = riemann_3(soln%V(1,i_low),&
+                         soln%V(2,i_low),soln%asnd(i_low))
+    tmp3(2)  = riemann_3(soln%V(1,i_low+1),&
+                         soln%V(2,i_low+1),soln%asnd(i_low+1))
 
     do i = i_low-1,ig_low,-1
       R0_ghost = two*tmp1(1) - tmp1(2)
@@ -319,7 +333,7 @@ contains
       call riem2prim(Rp_ghost,Rm_ghost,R0_ghost, &
             rho_ghost,uvel_ghost,avel_ghost,pres_ghost)
 
-      soln%V(i,:) = (/ rho_ghost, uvel_ghost, pres_ghost /)
+      soln%V(:,i) = (/ rho_ghost, uvel_ghost, pres_ghost /)
       soln%asnd(i) = avel_ghost
     end do
   end subroutine Left_sup_out_bndry
@@ -339,18 +353,18 @@ contains
     call speed_of_sound(pres,rho,avel)
 
     ! left supersonic inflow
-    tmp1(1)  = riemann_1(soln%V(i_high,1),&
-                         soln%V(i_high,3))
-    tmp1(2)  = riemann_1(soln%V(i_high-1,1),&
-                         soln%V(i_high-1,3))
-    tmp2(1)  = riemann_2(soln%V(i_high,1),&
-                         soln%V(i_high,2),soln%asnd(i_high))
-    tmp2(2)  = riemann_2(soln%V(i_high-1,1),&
-                         soln%V(i_high-1,2),soln%asnd(i_high-1))
-    tmp3(1)  = riemann_3(soln%V(i_high,1),&
-                         soln%V(i_high,2),soln%asnd(i_high))
-    tmp3(2)  = riemann_3(soln%V(i_high-1,1),&
-                         soln%V(i_high-1,2),soln%asnd(i_high-1))
+    tmp1(1)  = riemann_1(soln%V(1,i_high),&
+                         soln%V(3,i_high))
+    tmp1(2)  = riemann_1(soln%V(1,i_high-1),&
+                         soln%V(3,i_high-1))
+    tmp2(1)  = riemann_2(soln%V(1,i_high),&
+                         soln%V(2,i_high),soln%asnd(i_high))
+    tmp2(2)  = riemann_2(soln%V(1,i_high-1),&
+                         soln%V(2,i_high-1),soln%asnd(i_high-1))
+    tmp3(1)  = riemann_3(soln%V(1,i_high),&
+                         soln%V(2,i_high),soln%asnd(i_high))
+    tmp3(2)  = riemann_3(soln%V(1,i_high-1),&
+                         soln%V(2,i_high-1),soln%asnd(i_high-1))
 
     do i = i_high+1,ig_high
       R0_ghost = two*tmp1(1) - tmp1(2)
@@ -366,7 +380,7 @@ contains
       call riem2prim(Rp_ghost,Rm_ghost,R0_ghost, &
             rho_ghost,uvel_ghost,avel_ghost,pres_ghost)
 
-      soln%V(i,:) = (/ rho_ghost, uvel_ghost, pres_ghost /)
+      soln%V(:,i) = (/ rho_ghost, uvel_ghost, pres_ghost /)
       soln%asnd(i) = avel_ghost
     end do
   end subroutine Right_sup_out_bndry
