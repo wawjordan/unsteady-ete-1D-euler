1e0ee72ea5b59c22f7e907dc0cea3f33fa47716f
wawjordan <wajordan@vt.edu>
2021-09-29 18:50:03 -0400

    Moved build_LHS_matrix to residuals module and MUSCL_extrap to solution_reconstruction module

--- src/functions/other_subroutines.f90
+++ src/functions/other_subroutines.f90
@@ -6,7 +6,7 @@ module other_subroutines
   use set_inputs, only : epsM, kappaM
   use fluid_constants, only : gamma
   use variable_conversion
-  use flux_jacobians, only : VL_flux_jac
+  !use flux_jacobians, only : VL_flux_jac
   !use limiter_calc, only : limiter_fun!, calc_consecutive_variations
   use soln_type, only : soln_t
   use exact_soln_type, only : exact_soln_t
@@ -41,102 +41,10 @@ module other_subroutines
     source_term_1D_nozzle = pres*darea
   end function source_term_1D_nozzle
 
-  subroutine build_LHS_matrix(soln,grid)
-    type(soln_t), intent(inout) :: soln
-    type(grid_t), intent(in)    :: grid
-    real(prec), dimension(neq,i_low-1:i_high) :: Left, Right
-    real(prec), dimension(neq,neq) :: dfduim1, dfdui, dfduip1, dfduip2
-    real(prec), dimension(neq,neq) :: dgduim1, dgdui, dgduip1, dgduip2
-    real(prec) :: vt
-    integer :: i, j
-
-    call MUSCL_extrap(soln,Left,Right)
-    ! left boundary -> calculate dfdu_{i-1/2} wrt i, i+1, & i+2
-!    i = i_low-1
-!    call VL_flux_jac( Left(:,i), Right(:,i), &
-!            soln%U(:,i-1), soln%U(:,i), soln%U(:,i+1), soln%U(:,i+2), &
-!            soln%psi_p(:,i-1), soln%psi_p(:,i), &
-!            soln%psi_m(:,i), soln%psi_m(:,i+1), &
-!            soln%dpsi_p(:,i-1), soln%dpsi_p(:,i), &
-!            soln%dpsi_m(:,i), soln%dpsi_m(:,i+1), &
-!            dfduim1, dfdui, dfduip1, dfduip2 )
-!    soln%LHS(:,:,i+1,3) = -grid%Ai(i)*dfduim1    !A*df{i-1/2}/du{1}
-!    soln%LHS(:,:,i+1,4) = -grid%Ai(i)*dfdui  !A*df{i-1/2}/du{2}
-!    soln%LHS(:,:,i+1,5) = -grid%Ai(i)*dfduip1  !A*df{i-1/2}/du{3}
-    do i = i_low,i_high-1
-      !write(*,*) soln%psi_p(1,i), soln%psi_m(1,i)
-      call VL_flux_jac( Left(:,i), Right(:,i), &
-              soln%U(:,i-1), soln%U(:,i), soln%U(:,i+1), soln%U(:,i+2), &
-              soln%psi_p(:,i-1), soln%psi_p(:,i), &
-              soln%psi_m(:,i), soln%psi_m(:,i+1), &
-              soln%dpsi_p(:,i-1), soln%dpsi_p(:,i), &
-              soln%dpsi_m(:,i), soln%dpsi_m(:,i+1), &
-              dfduim1, dfdui, dfduip1, dfduip2 )
-      do j = 1,neq
-        write(*,*) dfdui(j,:)
-      end do
-      write(*,*)
-      vt = grid%Ac(i)*grid%dx(i)/soln%dt(i)
-      soln%LHS(:,:,i,1) = soln%LHS(:,:,i,1) + grid%Ai(i)*dfduim1
-      soln%LHS(:,:,i,2) = soln%LHS(:,:,i,2) + grid%Ai(i)*dfdui
-      soln%LHS(:,:,i,3) = vt + soln%LHS(:,:,i,3) + grid%Ai(i)*dfduip1
-      soln%LHS(:,:,i,4) = soln%LHS(:,:,i,4) + grid%Ai(i)*dfduip2
-
-      soln%LHS(:,:,i+1,2) = -grid%Ai(i)*dfduim1
-      soln%LHS(:,:,i+1,3) = -grid%Ai(i)*dfdui
-      soln%LHS(:,:,i+1,4) = -grid%Ai(i)*dfduip1
-      soln%LHS(:,:,i+1,5) = -grid%Ai(i)*dfduip2
-    end do
-!    i = i_high+1
-!    call VL_flux_jac( Left(:,i), Right(:,i), &
-!            soln%U(:,i-1), soln%U(:,i), soln%U(:,i+1), soln%U(:,i+2), &
-!            soln%psi_p(:,i-1), soln%psi_p(:,i), &
-!            soln%psi_m(:,i), soln%psi_m(:,i+1), &
-!            soln%dpsi_p(:,i-1), soln%dpsi_p(:,i), &
-!            soln%dpsi_m(:,i), soln%dpsi_m(:,i+1), &
-!            dfduim1, dfdui, dfduip1, dfduip2 )
-!      soln%LHS(:,:,i,1) = soln%LHS(:,:,i,1) + grid%Ai(i)*dfdui
-!      soln%LHS(:,:,i,2) = soln%LHS(:,:,i,2) + grid%Ai(i)*dfduip1
-!      soln%LHS(:,:,i,3) = soln%LHS(:,:,i,3) + grid%Ai(i)*dfduip2
-
-  end subroutine build_LHS_matrix
 
 
-  !================================ MUSCL_extrap =============================80
-  !>
-  !! Description:
-  !<
-  !===========================================================================80
-  subroutine MUSCL_extrap(soln,Left,Right)
-    type(soln_t), intent(inout) :: soln
-    real(prec), dimension(neq,i_low-1:i_high), intent(out) :: Left, Right
-    integer :: i
-    do i = i_low-1,i_high
-      Left(:,i) = soln%V(:,i) + fourth*epsM*( &
-            (one-kappaM)*soln%psi_p(:,i-1)*( soln%V(:,i) - soln%V(:,i-1) ) + &
-            (one+kappaM)*soln%psi_m(:,i)  *( soln%V(:,i+1) - soln%V(:,i) ) )
 
-      Right(:,i) = soln%V(:,i+1) - fourth*epsM*( &
-            (one-kappaM)*soln%psi_m(:,i+1)*( soln%V(:,i+2) - soln%V(:,i+1) ) + &
-            (one+kappaM)*soln%psi_p(:,i)  *( soln%V(:,i+1) - soln%V(:,i) ) )
-    end do
-
-   ! Left  = soln%V(:,i_low-1:i_high) + fourth*epsM*(       &
-   !         (one-kappaM)*soln%psi_p(:,i_low-2:i_high-1)* &
-   !                           ( soln%V(:,i_low-1:i_high)  &
-   !                           - soln%V(:,i_low-2:i_high-1) )&
-   !       + (one+kappaM)*soln%psi_m(:,i_low-1:i_high)* &
-   !                           ( soln%V(:,i_low  :i_high+1)  &
-   !                           - soln%V(:,i_low-1:i_high) ))
-   ! Right  = soln%V(:,i_low  :i_high+1) - fourth*epsM*(       &
-   !         (one-kappaM)*soln%psi_m(:,i_low  :i_high+1)* &
-   !                           ( soln%V(:,i_low+1:i_high+2)  &
-   !                           - soln%V(:,i_low  :i_high+1) )&
-   !       + (one+kappaM)*soln%psi_p(:,i_low-1:i_high)* &
-   !                           ( soln%V(:,i_low  :i_high+1)  &
-   !                           - soln%V(:,i_low-1:i_high) ))
-
-  end subroutine MUSCL_extrap
+  
 
   !================================== calc_de ==== ===========================80
   !>
