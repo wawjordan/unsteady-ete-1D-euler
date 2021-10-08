0e915643fab525d9254e598cb95edc0869477499
wawjordan <wajordan@vt.edu>
2021-10-08 11:01:27 -0400

    Changed dimensions for r_plus, r_minus, psi_plus, psi_minus, dpsi_plus, and dpsi_minus Initialized aforementioned arrays to zero Extrapolated consecutive variations at endpoints (r_plus and r_minus) Changed dimension order for soln%dudL and soln%dudR

--- src/functions/solution_reconstruction.f90
+++ src/functions/solution_reconstruction.f90
@@ -1,7 +1,7 @@
 module solution_reconstruction
 
   use set_precision,       only : prec
-  use set_constants,       only : small, zero, one, fourth
+  use set_constants,       only : small, zero, one, two, fourth
   use set_inputs,          only : neq, i_low, i_high, ig_low, ig_high, &
                                   epsM, kappaM
   use fluid_constants,     only : gamma
@@ -24,15 +24,25 @@ module solution_reconstruction
   subroutine MUSCL_extrap(soln,UL,UR)
     type(soln_t), intent(inout) :: soln
     real(prec), intent(out), dimension(neq,i_low-1:i_high) :: UL, UR
-    real(prec), dimension(neq,i_low-1:i_high) ::    r_plus,    r_minus, &
+    real(prec), dimension(neq,i_low-1:i_high+1) ::    r_plus,    r_minus, &
                                                   psi_plus,  psi_minus, &
                                                  dpsi_plus, dpsi_minus
+    !real(prec), dimension(neq,i_low-1:i_high) ::    r_plus,    r_minus, &
+    !                                              psi_plus,  psi_minus, &
+    !                                             dpsi_plus, dpsi_minus
     real(prec) :: den(neq), e4, km1, kp1
     integer :: i, j, k
 
     e4  = fourth*epsM
     km1 = one - kappaM
     kp1 = one + kappaM
+
+    r_plus     = zero
+    r_minus    = zero
+    psi_plus   = zero
+    psi_minus  = zero
+    dpsi_plus  = zero
+    dpsi_minus = zero
 
     ! Calculate consecutive variations
     i = i_low-1
@@ -47,27 +57,43 @@ module solution_reconstruction
       r_minus(:,i) = ( soln%U(:,i) - soln%U(:,i-1) )/den
     end do
 
+
     i = i_high+1
     den = soln%U(:,i+1) - soln%U(:,i)
     den = sign(one,den)*max(abs(den),small)
     r_minus(:,i) = ( soln%U(:,i) - soln%U(:,i-1) )/den
+    ! Extrapolate consecutive variations at endpoints
+    r_plus(:,i) = two*r_plus(:,i-1) - r_plus(:,i-2)
+    i = i_low-1
+    r_minus(:,i) = two*r_minus(:,i+1) - r_minus(:,i+2)
 
     ! Calculate limiters
     call soln%lim%limiter_fun(r_plus,psi_plus,dpsi_plus)
     call soln%lim%limiter_fun(r_minus,psi_minus,dpsi_minus)
+
+
+    !do i = i_low-1,i_high+1
+    !  write(*,*) i, psi_minus(:,i)
+    !end do
+    !write(*,*)
+    !do i = i_low-1,i_high+1
+    !  write(*,*) i, psi_plus(:,i)
+    !end do
+    !stop
+
 
     ! Left states & derivatives
     do  i = i_low,i_high+1
       UL(:,i-1) = soln%U(:,i) + e4*(                                         &
            km1*psi_plus(:,i-1)*( soln%U(:,i) - soln%U(:,i-1) )               &
          + kp1*psi_minus(:,i)*( soln%U(:,i+1) - soln%U(:,i) ) )
-      soln%duduL(1,:,i-1) = e4*(                                             &
+      soln%duduL(:,1,i-1) = e4*(                                             &
            km1*( dpsi_plus(:,i-1)*r_plus(:,i-1) - psi_plus(:,i-1) )          &
          - kp1*( dpsi_minus(:,i) ) )
-      soln%duduL(2,:,i-1) = one - e4*(                                       &
+      soln%duduL(:,2,i-1) = one - e4*(                                       &
            km1*( dpsi_plus(:,i-1)*(r_plus(:,i-1)+one) - psi_plus(:,i-1) )    &
          - kp1*( dpsi_minus(:,i)*(r_minus(:,i)+one) - psi_minus(:,i) ) )
-      soln%duduL(3,:,i-1) = e4*(                                             &
+      soln%duduL(:,3,i-1) = e4*(                                             &
            km1*( dpsi_plus(:,i-1) )                                          &
          - kp1*( dpsi_minus(:,i)*r_minus(:,i) - psi_minus(:,i) ) )
     end do
@@ -77,13 +103,13 @@ module solution_reconstruction
       UR(:,i) = soln%U(:,i+1) - e4*(                                         &
            km1*psi_minus(:,i+1)*( soln%U(:,i+2) - soln%U(:,i+1) )            &
          + kp1*psi_plus(:,i)*( soln%U(:,i+1) - soln%U(:,i) ) )
-      soln%duduR(1,:,i) = e4*(                                               &
+      soln%duduR(:,1,i) = e4*(                                               &
            km1*( dpsi_minus(:,i+1) )                                         &
          - kp1*( dpsi_plus(:,i)*r_plus(:,i) - psi_plus(:,i) ) )
-      soln%duduR(2,:,i) = one - e4*(                                         &
+      soln%duduR(:,2,i) = one - e4*(                                         &
            km1*( dpsi_minus(:,i+1)*(r_minus(:,i+1)+one) - psi_minus(:,i+1) ) &
          - kp1*( dpsi_plus(:,i)*(r_plus(:,i)+one) - psi_plus(:,i) ) )
-      soln%duduR(3,:,i) = e4*(                                               &
+      soln%duduR(:,3,i) = e4*(                                               &
            km1*( dpsi_minus(:,i+1)*r_minus(:,i+1) - psi_minus(:,i+1) )       &
          - kp1*( dpsi_plus(:,i) ) )
     end do
