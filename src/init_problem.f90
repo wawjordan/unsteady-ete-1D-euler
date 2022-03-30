115e3c2cb8b0f8214b0e020f0e4edcb735de5191
wawjordan <wajordan@vt.edu>
2022-03-30 16:13:28 -0400

    Changed initialization back to realizable values

--- src/init_problem.f90
+++ src/init_problem.f90
@@ -39,22 +39,22 @@ module init_problem
   !    soln%V(2,:) = U_R(2)
   !    soln%V(3,:) = U_R(3)
   !  end where
-  !  soln%V(1,:) = one-0.1_prec*grid%xc**2
-  !  soln%V(2,:) = grid%xc**2-1
-  !  soln%V(3,:) = one-0.2_prec*grid%xc**2
-    do i = ig_low, ig_high
-      soln%V(1,i) = real(i,prec)
-      soln%V(2,i) = real(i,prec)
-      soln%V(3,i) = real(i,prec)
-    end do
-    do i = ig_low, ig_high
-      soln%U(1,i) = real(i,prec)
-      soln%U(2,i) = real(i,prec)
-      soln%U(3,i) = real(i,prec)
-    end do
+    soln%V(1,:) = one-0.1_prec*grid%xc**2
+    soln%V(2,:) = grid%xc**2-1
+    soln%V(3,:) = one-0.2_prec*grid%xc**2
+    !do i = ig_low, ig_high
+    !  soln%V(1,i) = real(i,prec)
+    !  soln%V(2,i) = real(i,prec)
+    !  soln%V(3,i) = real(i,prec)
+    !end do
+    !do i = ig_low, ig_high
+    !  soln%U(1,i) = real(i,prec)
+    !  soln%U(2,i) = real(i,prec)
+    !  soln%U(3,i) = real(i,prec)
+    !end do
 
-    !call prim2cons(soln%U,soln%V)
-    !call update_states( soln )
+    call prim2cons(soln%U,soln%V)
+    call update_states( soln )
 
 
   end subroutine initialize
