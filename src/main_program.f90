84767ab9a1f575940728239aa66f75a4f604ac4d
wawjordan <wajordan@vt.edu>
2022-04-06 12:50:50 -0400

    Moved some code around for improved readability Changed initial condition to have 0 velocity Added multiple debug write statements Uncommented most of the iteration loop

--- src/main_program.f90
+++ src/main_program.f90
@@ -1,5 +1,7 @@
 program main_program
 
+  use dispmodule
+
   use set_constants, only : set_derived_constants, zero, one
   use fluid_constants, only : set_fluid_constants
   use set_inputs, only : set_derived_inputs, xmin, xmax, neq
@@ -35,12 +37,16 @@ program main_program
   type( exact_soln_t ) :: ex_soln
   real(prec), dimension(3) :: VL, VR, f
 
+  100 format(1(I0.8),3(G20.7))
+  write(header_str1,*) " Iter  |   ||density||    |"// &
+  & "   ||velocity||    |   ||pressure||    |"
+
   i = 0
   j = 0
   pnorm = 1
 
-  VL = (/one,0.1_prec,one/)
-  VR = (/0.125_prec,0.1_prec,0.1_prec/)
+  VL = (/one,zero,one/)
+  VR = (/0.125_prec,zero,0.1_prec/)
 
   call set_derived_constants
   call set_fluid_constants
@@ -51,40 +57,50 @@ program main_program
   call select_flux()
   call setup_exact_soln( ex_soln, grid, VL, VR)
   call initialize(grid,soln,VL,VR)
+  !do i = i_low,i_high
+  !  write(*,*) (soln%V(j,i),j=1,neq), ' | ', (soln%U(j,i),j=1,neq)
+  !end do
+  call output_soln(grid,soln,ex_soln,0)
 
   open(50,file='temp.txt',status='unknown')
 
-  100 format(1(I0.8),3(G20.7))
-
-  write(header_str1,*) " Iter  |   ||density||    |"// &
-  & "   ||velocity||    |   ||pressure||    |"
-  !call output_soln(grid,soln,ex_soln,0)
-  !call build_tridiag_test(soln)
-  !call build_LHS_test(soln)
+  call calc_time_step(grid%dx,soln%V,soln%asnd,soln%lambda,soln%dt)
   call build_LHS_matrix_1st_order(soln,grid)
-  !stop
- ! call block_band2full(soln%LHS,A_out,2,2,3,11)
- ! do i = 1,imax*neq
- !     write(*,*) (A_out(i,j), j = 1,imax*neq)
+  call calc_flux_1D(grid,soln)
+  call explicit_characteristic_bndry(soln, VL, VR)
+  call calc_residual(grid,soln)
+  do i = i_low,i_high
+    write(*,*) (soln%R(j,i),j=1,neq), ' | ', (soln%R(j,i),j=1,neq)
+  end do
+  stop
+
+  call implicit_euler(grid,soln)
+  call update_states( soln )
+  call output_soln(grid,soln,ex_soln,1)
+ ! write(*,*) i_low, i_high
+ ! write(*,*)
+
+ ! write(*,*) lbound(soln%LHS,1), ubound(soln%LHS,1)
+ ! write(*,*) lbound(soln%LHS,2), ubound(soln%LHS,2)
+ ! write(*,*) lbound(soln%LHS,3), ubound(soln%LHS,3)
+ ! write(*,*) lbound(soln%LHS,4), ubound(soln%LHS,4)
+ ! write(*,*)
+ ! write(*,*) lbound(soln%V,1), ubound(soln%V,1)
+ ! write(*,*) lbound(soln%V,2), ubound(soln%V,2)
+
+ ! write(*,*)
+
+ ! do i = i_low,i_high
+ !   do j = 1,2
+ !     call disp('      ',soln%LHS(:,:,i,j), advance='NO')
+ !   end do
+ !   call disp('      ',soln%LHS(:,:,i,3))
  ! end do
 
-  call calc_residual(grid,soln)
-
-  call calc_time_step(grid%dx,soln%V,soln%asnd,soln%lambda,soln%dt)
-  !stop
-!!!  !call explicit_euler(grid,soln%S,soln%dt,soln%F,soln%U,soln%R)
-  call explicit_characteristic_bndry(soln, VL, VR)
-  !stop
-!!!  !call explicit_RK(grid,soln,VL,VR)
-  call implicit_euler(grid,soln)
-  !stop
-  call update_states( soln )
   !stop
 !!!  write(*,*) minval(soln%dt)
 !!!  soln%time = soln%time + minval(soln%dt)
   !call sample_exact_soln(ex_soln,grid,soln%time)
-  !stop
-  call output_soln(grid,soln,ex_soln,1)
   call residual_norms(soln%R,soln%rinit,(/one,one,one/))
   soln%rold = soln%rinit
   write(*,*) 'Residual Norms: Iteration 0'
@@ -93,11 +109,16 @@ program main_program
   write(*,*)
   !stop
   write(*,*) 'Relative Residual Norms:'
-  do j = 2,5 !max_iter
+  do j = 2,max_iter
 !!!    !call calculate_sources(soln,grid)
-    call build_LHS_matrix_1st_order(soln,grid)
     call calc_time_step(grid%dx,soln%V,soln%asnd,soln%lambda,soln%dt)
+    call build_LHS_matrix_1st_order(soln,grid)
+    call calc_flux_1D(grid,soln)
+    call explicit_characteristic_bndry(soln, VL, VR)
+    call calc_residual(grid,soln)
     call implicit_euler(grid,soln)
+    call update_states( soln )
+    !call output_soln(grid,soln,ex_soln,j)
     soln%time = soln%time + minval(soln%dt)
     !call sample_exact_soln(ex_soln,grid,soln%time)
     if (mod(j,soln_save)==0) then
