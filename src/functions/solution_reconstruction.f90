9166591d533163d2ec1eec978d0d9cf617397737
wawjordan <wajordan@vt.edu>
2022-04-06 12:46:08 -0400

    Changed MUSCL_extrap_lite to use primitive variables Implementation needs to be verified again

--- src/functions/solution_reconstruction.f90
+++ src/functions/solution_reconstruction.f90
@@ -119,15 +119,21 @@ module solution_reconstruction
 
   !============================ MUSCL_extrap_lite ============================80
   !>
-  !! Description:
+  !! Description: MUSCL extrapolation for primitive variables
   !<
   !===========================================================================80
-  subroutine MUSCL_extrap_lite(soln,UL,UR)
+  subroutine MUSCL_extrap_lite(soln,VL,VR)
     type(soln_t), intent(inout) :: soln
-    real(prec), intent(out), dimension(neq,i_low-1:i_high) :: UL, UR
+    real(prec), intent(out), dimension(neq,i_low-1:i_high) :: VL, VR
     real(prec), dimension(neq,i_low-1:i_high) ::    r_plus,    r_minus, &
                                                   psi_plus,  psi_minus, &
                                                  dpsi_plus, dpsi_minus
+    !real(prec), dimension(neq,i_low-1:i_high) ::    r_plus, &
+    !                                              psi_plus, &
+    !                                             dpsi_plus
+    !real(prec), dimension(neq,i_low:i_high+1) ::   r_minus, &
+    !                                             psi_minus, &
+    !                                            dpsi_minus
     real(prec) :: den(neq), e4, km1, kp1
     integer :: i, j, k
 
@@ -137,21 +143,21 @@ module solution_reconstruction
 
     ! Calculate consecutive variations
     i = i_low-1
-    den = soln%U(:,i+1) - soln%U(:,i)
+    den = soln%V(:,i+1) - soln%V(:,i)                      ! u_{i+1} - u_{i}
     den = sign(one,den)*max(abs(den),small)
-    r_plus(:,i) = ( soln%U(:,i+2) - soln%U(:,i+1) )/den
+    r_plus(:,i) = ( soln%V(:,i+2) - soln%V(:,i+1) )/den    ! r+_{i+1/2}
 
     do  i = i_low,i_high
-      den = soln%U(:,i+1) - soln%U(:,i)
+      den = soln%V(:,i+1) - soln%V(:,i)                    ! u_{i+1} - u_{i}
       den = sign(one,den)*max(abs(den),small)
-      r_plus(:,i)  = ( soln%U(:,i+2) - soln%U(:,i+1) )/den
-      r_minus(:,i) = ( soln%U(:,i) - soln%U(:,i-1) )/den
+      r_plus(:,i)  = ( soln%V(:,i+2) - soln%V(:,i+1) )/den ! r+_{i+1/2}
+      r_minus(:,i-1) = ( soln%V(:,i) - soln%V(:,i-1) )/den   ! r-_{i+1/2}
     end do
 
     i = i_high+1
-    den = soln%U(:,i+1) - soln%U(:,i)
+    den = soln%V(:,i+1) - soln%V(:,i)                      ! u_{i+1} - u_{i}
     den = sign(one,den)*max(abs(den),small)
-    r_minus(:,i) = ( soln%U(:,i) - soln%U(:,i-1) )/den
+    r_minus(:,i-1) = ( soln%V(:,i) - soln%V(:,i-1) )/den     ! r-_{i+1/2}
 
     ! Calculate limiters
     call soln%lim%limiter_fun(r_plus,psi_plus,dpsi_plus)
@@ -159,17 +165,22 @@ module solution_reconstruction
 
     ! Left states & derivatives
     do  i = i_low,i_high+1
-      UL(:,i-1) = soln%U(:,i) + e4*(                                         &
-           km1*psi_plus(:,i-1)*( soln%U(:,i) - soln%U(:,i-1) )               &
-         + kp1*psi_minus(:,i)*( soln%U(:,i+1) - soln%U(:,i) ) )
+      VL(:,i-1) = soln%V(:,i) + e4*(                                         &
+           km1*psi_plus(:,i-1)*( soln%V(:,i) - soln%V(:,i-1) )               &
+         + kp1*psi_minus(:,i)*( soln%V(:,i+1) - soln%V(:,i) ) )
     end do
 
     ! Right states & derivatives
     do i = i_low-1,i_high
-      UR(:,i) = soln%U(:,i+1) - e4*(                                         &
-           km1*psi_minus(:,i+1)*( soln%U(:,i+2) - soln%U(:,i+1) )            &
-         + kp1*psi_plus(:,i)*( soln%U(:,i+1) - soln%U(:,i) ) )
+      VR(:,i) = soln%V(:,i+1) - e4*(                                         &
+           km1*psi_minus(:,i+1)*( soln%V(:,i+2) - soln%V(:,i+1) )            &
+         + kp1*psi_plus(:,i)*( soln%V(:,i+1) - soln%V(:,i) ) )
     end do
+
+    !do i = i_low-1,i_high
+    !  write(*,*) (UL(j,i),j=1,neq), '  ', (UR(j,i),j=1,neq)
+    !end do
+    !stop
 
   end subroutine MUSCL_extrap_lite
 
